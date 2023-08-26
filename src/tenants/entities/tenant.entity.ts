f17194d1695cf10de432bb3e4fbdd5c61efbccd2
Mostafa1201 <mostafa1201@hotmail.com>
2023-08-26 17:40:58 +0300

    # feat - Add some validation for tenant model

--- src/tenants/entities/tenant.entity.ts
+++ src/tenants/entities/tenant.entity.ts
@@ -1,1 +1,25 @@
-export class Tenant {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { IsEmail, IsMobilePhone, IsNotEmpty, Length } from "@nestjs/class-validator";
+
+@Entity()
+export class Tenant {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    @Length(2, 30, { message: 'fullName must be at least 2 but not longer than 30 characters' })
+    @IsNotEmpty({ message: 'fullName is required' })
+    fullName: string;
+
+    @Column()
+    @IsEmail({}, { message: 'Incorrect email' })
+    @IsNotEmpty({ message: 'email is required' })
+    email: string;
+
+    @Column()
+    @IsMobilePhone()
+    @IsNotEmpty({ message: 'phone is required' })
+    phone: string;
+
+}
